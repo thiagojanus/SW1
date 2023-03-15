a58354b4f1f1c8fdc2089ea3191d25dee704d482
projetojanuario <100379765+thiagojanus@users.noreply.github.com>
2023-03-15 16:38:07 -0300

    Add files via upload

---
+++ exercicioAlunoMedia/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exercicioAlunoMedia;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Thiago";
+        aluno1.nota1 = 9.2;
+        aluno1.nota2 = 6.8;
+
+        aluno1.mensagem();
+        
+
+        
+
+    }
+}
